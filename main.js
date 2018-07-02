6d912f90d08d7844efd62fa7ffe29e63d5473ebb
jhassler1031 <jhassler1031@gmail.com>
2018-07-02 13:10:14 -0400

    setup the progress bar

--- main.js
+++ main.js
@@ -13,8 +13,9 @@ function getTime() {
   //Sets today to the current date/time
   let today = new Date();
 
-  //Sets clock to the html element of class clock
+  //Get HTML elements to modify
   let clock = document.querySelector(".clock");
+  let timerBar = document.querySelector(".timer-bar");
 
   //.getHours() used on Date object to get hours
   let h = today.getHours();
@@ -32,13 +33,24 @@ function getTime() {
 
   //.getSeconds() used to get seconds from Date object
   let s = today.getSeconds();
+  //Calculate the percentage of a minute the seconds represent
+  let percentOfMinute = (s/60) * 100;
+  //Round the decimal to the nearest whole number
+  percentOfMinute = Math.round(percentOfMinute);
+
   //If number is less than 10, add a 0 so all times have two digits
   if (s < 10) {
     s = "0" + s;
   }
 
   console.log(`The time is ${h}:${m}:${s}`);
+  console.log(percentOfMinute);
+
+  //Determine the length of the progress bar, cutting percentOfMinute in half so the bar doesn't get too long
+  let bar = "_".repeat(percentOfMinute / 2);
+  console.log(bar);
 
   //Adds the time to the html to display on the page
   clock.textContent = `${h}:${m}:${s}`
+  timerBar.textContent = bar;
 }
