1f0fab6b2225d2ba5a34e6cf0e59cd498f3a0fa0
jhassler1031 <jhassler1031@gmail.com>
2018-07-02 13:57:15 -0400

    got the background color changing by the second

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function getTime() {
   //Get HTML elements to modify
   let clock = document.querySelector(".clock");
   let timerBar = document.querySelector(".timer-bar");
+  let background = document.querySelector(".clock-container");
 
   //.getHours() used on Date object to get hours
   let h = today.getHours();
@@ -33,10 +34,20 @@ function getTime() {
 
   //.getSeconds() used to get seconds from Date object
   let s = today.getSeconds();
+
   //Calculate the percentage of a minute the seconds represent
   let percentOfMinute = (s/60) * 100;
   //Round the decimal to the nearest whole number
   percentOfMinute = Math.round(percentOfMinute);
+
+  //Get the hex of seconds using the toString() method
+  let hexNumber = s.toString(16);
+  //If the hex is single digit, add a 0 to the beginning
+  if (hexNumber.length < 2) {
+    hexNumber = "0" + hexNumber;
+  }
+  //Add the beginning of the hex color to the variable
+  hexNumber = "#F088" + hexNumber;
 
   //If number is less than 10, add a 0 so all times have two digits
   if (s < 10) {
@@ -45,12 +56,17 @@ function getTime() {
 
   console.log(`The time is ${h}:${m}:${s}`);
   console.log(percentOfMinute);
+  console.log(hexNumber);
 
   //Determine the length of the progress bar, cutting percentOfMinute in half so the bar doesn't get too long
+  //.repeat(number of times tor repeat) is a function that can be called on a string
   let bar = "_".repeat(percentOfMinute / 2);
   console.log(bar);
 
   //Adds the time to the html to display on the page
   clock.textContent = `${h}:${m}:${s}`
+  //Adjust the progress bar
   timerBar.textContent = bar;
+  //Change the background color
+  background.style.backgroundColor = hexNumber;
 }
